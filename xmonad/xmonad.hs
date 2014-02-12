fba19974d3469f45772fa452898e5916ecd3f2c2
José Fernández Ramos <el.atomo@gmail.com>
2014-02-12 22:44:26 +0100

    Fix xmonad and intellij + gitignore.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,10 +5,12 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.SetWMName
 
 main=do
     xmonad $ defaultConfig
         { borderWidth = 1
         , manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $ smartBorders $  layoutHook defaultConfig
+        , startupHook = setWMName "LG3D"
         }
