a4dd7750734690406113823f7b753617734c4a19
José Fernández Ramos <el.atomo@gmail.com>
2014-12-07 14:18:37 +0100

    Little script for converting videos to xvoid

---
+++ bin/avify.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Converts video file to avi:
+#    avify.sh <file>...
+
+for file in "$@"; do
+    filename="${file%.*}"
+    extension="${file##*.}"
+
+    ffmpeg -i "$file" -vcodec libxvid -acodec ac3 -ar 48000 -ab 192k "$filename.avi"
+done
