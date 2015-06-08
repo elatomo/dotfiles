a647d0070ce4da5f066389df972a16cd2e6b2dcf
José Fernández Ramos <el.atomo@gmail.com>
2015-06-08 22:32:18 +0200

    surfddg.sh: :)

---
+++ bin/surfddg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Surf duckduckgo.com
+#    surfddg.sh [QUERY]
+
+BASE_URL="https://www.duckduckgo.com"
+
+# no extra arguments
+if [ $# -eq 0 ]; then
+    surf $BASE_URL
+else
+    # treat arguments as search query
+    query=$(printf "+%s" "$@")
+    query=${query:1}
+    surf "https://www.duckduckgo.com?q=$query"
+fi
+
+exit $?
