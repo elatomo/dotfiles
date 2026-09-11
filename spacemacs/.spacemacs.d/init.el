95981596849ed56a9197adb74022ce0e8c6eba1e
José Fernández Ramos <jose@navika.io>
2026-09-11 12:30:36 +0200

    spacemacs: add terraform layer

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -92,6 +92,7 @@ This function should only modify configuration layer settings."
      janet
      csv
      php
+     terraform
      )
 
    ;; List of additional packages that will be installed without being wrapped
